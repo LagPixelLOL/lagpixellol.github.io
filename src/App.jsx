20a272ee0e445e72c6ac3897c41615c7b866f8e5
v2ray <2282688304@qq.com>
2026-01-14 15:39:23 +0000

    Started redo of my front page, got SimpleBar to work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,11 @@
-import Card from "./Card.jsx"
-import FullBar from "./FullBar.jsx"
+import SimpleBar from "simplebar-react"
 import {useState, useEffect} from "react"
-import decodeAPNG from "./apng_decoder.js"
 import boykisserImg from "./assets/boykisser.avif"
-import backgroundImg from "./assets/background.png"
 
 function App() {
-    const [frameQueue, setFrameQueue] = useState([]);
-
-    useEffect(() => {
-        let stop = false;
-        const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-        (async function initAnimation() {
-            const res = await decodeAPNG(backgroundImg);
-            let i = 0;
-            while (!stop && res.length) {
-                setFrameQueue(prev => {
-                    let newFrameQueue = [res[i]];
-                    if (prev[0]) {
-                        newFrameQueue.push(prev[0]);
-                    }
-                    return newFrameQueue
-                });
-                const delay = res[i].delayMs ?? 100;
-                i = (i + 1) % res.length;
-                await sleep(delay);
-            }
-        })();
-        return () => {stop = true}
-    }, []);
-
-    const current = frameQueue[0]?.url;
-    const previous = frameQueue[1]?.url;
-
     return (
-        <div
-            className="flex flex-col overflow-x-hidden overflow-y-auto w-dvw h-dvh bg-cover bg-scroll bg-center [image-rendering:pixelated]"
-            style={current ? {backgroundImage: `url(${current}), url(${previous || backgroundImg})`} : {backgroundImage: `url(${backgroundImg})`}}
-        >
-            <div className="flex w-full p-[1rem]">
-                <FullBar>
-                    <p className="font-extrabold">
-                        Hello, world! :3
-                    </p>
-                </FullBar>
-            </div>
-            <div className="flex w-full flex-wrap gap-[1rem] px-[1rem]">
-                <Card>My username is typically v2ray, also known as LagPixelLOL.</Card>
-                <Card>
-                    <a href="https://chat.boikisser.com"><img className="rounded-[10px] h-[6rem]" src={boykisserImg}></img></a>
-                </Card>
-                <Card>
-                    <p>
-                        Email: 2282688304@qq.com<br/>
-                        GitHub: <a href="https://github.com/LagPixelLOL" target="_blank"><span className="link-text">LagPixelLOL</span></a><br/>
-                        HuggingFace: <a href="https://huggingface.co/v2ray" target="_blank"><span className="link-text">v2ray</span></a><br/>
-                        Discord: <a href="https://discord.gg/r4Wj97nZ" target="_blank"><span className="link-text">@v2ray</span></a>
-                    </p>
-                </Card>
-                <Card>Lead of Project Looking Glass, operator of Straylight, searching for Rainbows in Starlights.</Card>
-            </div>
-            <footer className="text-center mt-auto py-2.5 font-mono">Made with GEX && React && Tailwind</footer>
-        </div>
+        <SimpleBar autoHide={false} className="w-dvw h-dvh overflow-x-none simplebar-thin-white">
+        </SimpleBar>
     )
 }
 
