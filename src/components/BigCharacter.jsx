a92622ef9ca59ea86032c402d7b7076fcead1e0c
v2ray <2282688304@qq.com>
2026-01-17 17:43:05 +0000

    Prevented selection BigCharacter.

--- src/components/BigCharacter.jsx
+++ src/components/BigCharacter.jsx
@@ -5,7 +5,7 @@ export default function BigCharacter({className, children, bgImageStyle, ...prop
     const [isHovered, setIsHovered] = useState(false);
 
     const wrapperCommonClassName = "absolute inset-0 flex justify-center items-center overflow-hidden";
-    const textCommonClassName = "text-[85cqw] font-pixel-l text-shadow-none text-transparent bg-transparent select-none pointer-events-none translate-x-[3.5cqw] -translate-y-[4cqw]";
+    const spanCommonClassName = "translate-x-[3.5cqw] -translate-y-[4cqw] before:content-[attr(data-text)] before:font-pixel-l before:text-[85cqw] before:text-transparent before:text-shadow-none";
 
     return (
         <div
@@ -15,10 +15,17 @@ export default function BigCharacter({className, children, bgImageStyle, ...prop
             {...props}
         >
             <div className={wrapperCommonClassName}>
-                <p style={{backgroundImage: bgImageStyle, opacity: isHovered ? 1 : 0}} className={`${textCommonClassName} [-webkit-background-clip:text] transition-opacity duration-150`}>{children}</p>
+                <span 
+                    data-text={children}
+                    style={{'--bg-image': bgImageStyle, opacity: isHovered ? 1 : 0}} 
+                    className={`${spanCommonClassName} before:[background-image:var(--bg-image)] before:[-webkit-background-clip:text] transition-opacity duration-150`}
+                />
             </div>
             <div className={wrapperCommonClassName}>
-                <p className={`${textCommonClassName} [-webkit-text-stroke:3cqw_rgb(248,_248,_248)]`}>{children}</p>
+                <span 
+                    data-text={children}
+                    className={`${spanCommonClassName} before:[-webkit-text-stroke:3cqw_rgb(248,_248,_248)]`}
+                />
             </div>
         </div>
     );
